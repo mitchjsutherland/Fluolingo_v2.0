eac65bb5b5f22ceede49f2d75ef9c61f3ae8a0cf
Mitch Sutherland <mitchelsutherland@icloud.com>
2024-03-23 14:11:54 +0000

    Home logo and start button animated

--- Fluoling frontend (Client)/src/Home/Home.jsx
+++ Fluoling frontend (Client)/src/Home/Home.jsx
@@ -10,10 +10,10 @@ function Home() {
 
     <div className="body">
       <div className='homeContainer'>
-        <motion.img animate={{rotate: [null, 720]}} whileHover={{scale: [null, 1.2]}} className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
+        <motion.img initial={{scale: 0}} animate={{scale: 1, rotate: [0, 1083, 1079, 1080]}} transition={{duration: 1, times: [0, 0.87, 0.92, 0.95]}} whileHover={{scale: [null, 1.2]}} className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
         <div>
         <Link to="/users/login">
-          <Button variant="primary" className="g-button homeStartButton">Click to start</Button>  
+          <motion.Button initial={{opacity: 0}} animate={{opacity: [0, 0, 0.5 ,1]}} transition={{duration: 4, times: [0, 0.5, 0.7, 1]}} variant="primary" className="g-button homeStartButton">Click to start</motion.Button>  
         </Link>
         </div>
       </div>
