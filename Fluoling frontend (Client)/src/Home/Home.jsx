a4cbe6a75a50da726794ca4937be4a031badac1f
Mitch Sutherland <mitchelsutherland@icloud.com>
2024-03-23 13:16:23 +0000

    Exploring home page animation

--- Fluoling frontend (Client)/src/Home/Home.jsx
+++ Fluoling frontend (Client)/src/Home/Home.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap'; 
+import { motion } from "framer-motion"
 import "./Homenew.css";
 
 function Home() {
 
   return (
+
     <div className="body">
       <div className='homeContainer'>
-        <img className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
+        <motion.img animate={{rotate: [null, 720]}} whileHover={{scale: [null, 1.2]}} className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
         <div>
         <Link to="/users/login">
           <Button variant="primary" className="g-button homeStartButton">Click to start</Button>  
         </Link>
         </div>
       </div>
-    
     </div>
+
   );
 }
 
