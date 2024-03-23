4b4335a5e9faf97d51f82ffcb6be721bf2053ad9
Mitch Sutherland <mitchelsutherland@icloud.com>
2024-03-23 16:00:26 +0000

    Specifying new transition duration for logo scale

--- Fluoling frontend (Client)/src/Home/Home.jsx
+++ Fluoling frontend (Client)/src/Home/Home.jsx
@@ -10,9 +10,10 @@ function Home() {
 
     <div className="body">
       <div className='homeContainer'>
-        <motion.img initial={{scale: 0}} animate={{scale: 1, rotate: [0, 1083, 1079, 1080]}} transition={{duration: 1, times: [0, 0.87, 0.92, 0.95]}} whileHover={{scale: [null, 1.2]}} className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
+        <motion.img initial={{scale: 0}} animate={{scale: 1, rotate: [0, 1082, 1079, 1080]}} transition={{duration: 1, times: [0, 0.87, 0.92, 0.95], scale: {duration: 1.5}}} whileHover={{scale: [null, 1.03]}} className="homeLogo" src="../public/FluoLogoHome.svg" alt="Logo" />
         <div>
         <Link to="/users/login">
+          {/* Orchestration may be used here to delay the animation of start button to follow logo entry? */}
           <motion.Button initial={{opacity: 0}} animate={{opacity: [0, 0, 0.5 ,1]}} transition={{duration: 4, times: [0, 0.5, 0.7, 1]}} variant="primary" className="g-button homeStartButton">Click to start</motion.Button>  
         </Link>
         </div>
